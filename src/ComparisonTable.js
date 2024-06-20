27bd4234216a2cc9ae9a6bfaab900d02883539da
unknown <jghuynh3@gmail.com>
2024-06-20 14:02:16 -0700

    Removed unnecessary comments

--- src/ComparisonTable.js
+++ src/ComparisonTable.js
@@ -1,76 +1,12 @@
 import { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 
-function ComparisonTable({climbersList}) {
+function ComparisonTable({ climbersList }) {
   // Ensure climber1 is an array before calling map
   if (!Array.isArray(climbersList)) {
     return null;
   }
-  // console.log(`Climbers List: ${climbersList}`)
-  //  const [climber1, setclimber1] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("/climbers-partial.json")
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((climber1) => setclimber1(climber1))
-  //     .catch((error) =>
-  //       console.error(
-  //         "There has been a problem with your fetch operation:",
-  //         error
-  //       )
-  //     );
-  // }, []);
-  // const jsonObject = [
-  //   {
-  //     climber: "Janja GARNBRET",
-  //     nation: "SLO",
-  //     profileUrl: "https://www.ifsc-climbing.org/athlete/1147/janja-garnbret",
-  //     gender: "women",
-  //     age: "25",
-  //     association: "Alpine Association of Slovenia",
-  //     profileImage:
-  //       "https://www.ifsc-climbing.org/_next/image?url=https%3A%2F%2Fifsc.results.info%2Fathletes%2F1147%2Fphoto&w=384&q=75",
-  //     boulder: {
-  //       rank: "1",
-  //       points: "4805.0",
-  //     },
-  //     lead: {
-  //       rank: "1",
-  //       points: "5610.0",
-  //     },
-  //     "boulder-lead": {
-  //       rank: "1",
-  //       points: "7000.0",
-  //     },
-  //   },
-  //   {
-  //     climber: "Ai MORI",
-  //     nation: "JPN",
-  //     profileUrl: "https://www.ifsc-climbing.org/athlete/2380/ai-mori",
-  //     gender: "women",
-  //     age: "20",
-  //     association: "Japan Mountaineering & Sport Climbing Association",
-  //     profileImage:
-  //       "https://www.ifsc-climbing.org/_next/image?url=https%3A%2F%2Fifsc.results.info%2Fathletes%2F2380%2Fphoto&w=384&q=75",
-  //     lead: {
-  //       rank: "2",
-  //       points: "4610.0",
-  //     },
-  //     "boulder-lead": {
-  //       rank: "3",
-  //       points: "4535.0",
-  //     },
-  //     boulder: {
-  //       rank: "15",
-  //       points: "1535.0",
-  //     },
-  //   },
-  // ];
   return (
     <Table striped bordered hover>
       <thead>
