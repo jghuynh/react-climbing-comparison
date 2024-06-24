fc1f7c5a972f7984dcb8cfd878a65d58dd30ac26
unknown <jghuynh3@gmail.com>
2024-06-24 12:01:59 -0700

    Added documentation

--- src/Postcard.js
+++ src/Postcard.js
@@ -1,7 +1,24 @@
+/**
+ * A Postcard React component that displays a climber's statistics
+ */
 import Climber from "./Climber";
 import Card from "react-bootstrap/Card";
 
-
+/**
+ * Displays a climber's statistics
+ *
+ * @param {string} name the name of the athletes
+ * @param {int} age the athlete's age
+ * @param {string} gender the athlete's gender: male or female (no nonbinary--sorry!)
+ * @param {string} nation the athlete's nation; country of citizenship
+ * @param {object} boulder the athlete's boulder points and ranking
+ * @param {object} lead the athlete's lead points and ranking
+ * @param {object} speed the athlete's speed points and ranking
+ * @param {object} boulderLead the athlete's boulder-lead points and ranking
+ * @param {string} profileUrl the URL (string-form) to the athlete's profile page
+ * @param {string} profileIMage the URL (string-form) to the athlete's profile image
+ * @returns {ReactNode} the Postcard of basic athlete's statistics information
+ */
 function Postcard({
   name,
   age,
