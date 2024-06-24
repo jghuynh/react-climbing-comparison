d7a3b892ac67ebf6c6cada0a751ff302e6fc3086
unknown <jghuynh3@gmail.com>
2024-06-24 15:02:05 -0700

    Added documentation

--- src/Climber.js
+++ src/Climber.js
@@ -1,19 +1,38 @@
-function Climber({
-  // name,
-  age,
-  gender,
-  nation,
-  boulder,
-  lead,
-  boulderLead,
-  speed,
-}) {
+// Displays the climber's stats in a cleaner method
+import ReactCountryFlag from "react-country-flag";
+
+/**
+ * Displays the climber's stats, in a cleaner method
+ *
+ * @param {int} age the athlete's age
+ * @param {string} gender the athlete's gender
+ * @param {string} nation the athlete's nationality, ISO-style
+ * @param {object} boulder the athlete's boulder points and rank
+ * @param {object} lead the athlete's lead points and rank
+ * @param {object} boulderLead the athlete's boulder-lead points and rank
+ * @param {object} speed the athlete's speed points and rank
+ * @return {ReactNode} the Climber's stats
+ *
+ * @returns
+ */
+
+function Climber({ age, gender, nation, boulder, lead, boulderLead, speed }) {
   return (
     <div>
       {/* <h3>Name: {name}</h3> */}
       <p>Age: {age}</p>
       <p>Gender: {gender}</p>
       {/* https://github.com/danalloway/react-country-flag */}
+      <ReactCountryFlag
+        className="emojiFlag"
+        countryCode={nation}
+        svg
+        style={{
+          fontSize: "2em",
+          lineHeight: "2em",
+        }}
+        aria-label={nation}
+      />
       <p>Nationality: {nation}</p>
       {boulder && (
         <>
